404e21d2de1439ad55f88e160f483ec3833ff980
Artyom Kamenshchikov <armswort@gmail.com>
2011-05-24 23:58:54 +0300

    User model with passwords and user display page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 # == Schema Information
-# Schema version: 20110515194107
+# Schema version: 20110516205628
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
